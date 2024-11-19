f54480996b061cc31d178083551bb620d381fa11
diegocamargo55555 <diegocamargo55555@gmail.com>
2024-11-19 15:21:10 -0300

    mudança navbar - bea

--- src/components/Header/navbar.js
+++ src/components/Header/navbar.js
@@ -11,8 +11,13 @@ function Header() {
         <header>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Jim+Nightshade&display=swap" rel="stylesheet"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Jim+Nightshade&display=swap" rel="stylesheet"/>
+
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Jim+Nightshade&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
+
             <body className="body">
                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
                     <div class="container-fluid" className="header">
@@ -23,9 +28,9 @@ function Header() {
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li class="nav-item">
-                                    <Link class="nav-link active" aria-current="page" to='/Acessibilidade'><p className="item">Acessibilidade</p></Link>
+                                    <Link class="nav-link active" aria-current="page" to='/Acessibilidade'><p className="items acessibilidade">Acessibilidade</p></Link>
                                 </li>
-                                <NavDropdown title="Lista" id="basic-nav-dropdown">
+                                <NavDropdown title="Lista" id="basic-nav-dropdown" className="items lista">
                                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                     <NavDropdown.Item href="#action/3.2">
                                         Another action
@@ -37,7 +42,7 @@ function Header() {
                                     </NavDropdown.Item>
                                 </NavDropdown>
                                 <li class="nav-item">
-                                    <Link class="nav-link active" aria-current="page" to='/Pesquisa'>Pesquisa</Link>
+                                    <Link class="nav-link active" aria-current="page" to='/Pesquisa'><p className="items pesquisa">Pesquisa</p></Link>
                                 </li>
 
                             </ul>

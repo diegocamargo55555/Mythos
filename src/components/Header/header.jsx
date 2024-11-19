87a2c5cb9a2f80b647f6b60175a1b066438bde6a
Ana Beatriz <anabeatriz.sureke@gmail.com>
2024-11-19 11:09:17 -0300

    estilizando navbar

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Titulo = styled.h1`
+    text-align: center;
+    font-size: 96px;
+`;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,29 @@
 import { Link } from "react-router-dom";
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import "./header.css";
+import { Titulo } from "./header";
 
 
 function Header() {
+    
     return (
+        
         <header>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
-            <body>
-                <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-                    <div class="container-fluid">
-                        <Link class="navbar-brand" to='/'>Home</Link>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Jim+Nightshade&display=swap" rel="stylesheet"/>
+            <body className="body">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    <div class="container-fluid" className="header">
+                        <Link class="navbar-brand" to='/' className="titulo"><Titulo>Mythos</Titulo></Link>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li class="nav-item">
-                                    <Link class="nav-link active" aria-current="page" to='/Acessibilidade'>Acessibilidade</Link>
+                                    <Link class="nav-link active" aria-current="page" to='/Acessibilidade'><p className="item">Acessibilidade</p></Link>
                                 </li>
                                 <NavDropdown title="Lista" id="basic-nav-dropdown">
                                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>

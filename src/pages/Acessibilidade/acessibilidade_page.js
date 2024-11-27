5200723c1a4338420c9fdef07543301b08ebba2d
Ana Beatriz <anabeatriz.sureke@gmail.com>
2024-11-27 12:30:45 -0300

    inicio implementação leitura bionica

--- src/pages/Acessibilidade/acessibilidade_page.js
+++ src/pages/Acessibilidade/acessibilidade_page.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./acessibilidade.css"
+import { textVide } from 'text-vide';
 
 const Acessibilidade = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
@@ -32,7 +33,9 @@ const Acessibilidade = () => {
       window.location.reload();
     }
    
-
+    const text =
+    'Bionic Reading is a new method facilitating the reading process by guiding the eyes through text with artificial fixation points. As a result, the reader is only focusing on the highlighted initial letters and lets the brain center complete the word. In a digital world dominated by shallow forms of reading, Bionic Reading aims to encourage a more in-depth reading and understanding of written content.';
+    const highlightedText = textVide(text);
     return (
       <div className={isDarkMode ? "dark-mode" : "body-claro"}>
         <div className={isBig ? "bigText" : "smallText"}> 
@@ -50,7 +53,7 @@ const Acessibilidade = () => {
           <main>
           <div>
               <p>
-                  Acessibilidade
+                  {highlightedText}
               </p>
             </div>
           </main>

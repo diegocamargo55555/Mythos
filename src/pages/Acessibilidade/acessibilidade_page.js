e7f3d29f8c8d074bfd9587f749db89c63e10db62
Ana Beatriz <anabeatriz.sureke@gmail.com>
2024-11-27 12:23:42 -0300

    acessibilidade agora permite alteração tamanho do texto

--- src/pages/Acessibilidade/acessibilidade_page.js
+++ src/pages/Acessibilidade/acessibilidade_page.js
@@ -3,6 +3,7 @@ import "./acessibilidade.css"
 
 const Acessibilidade = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
+    const[isBig, setisBig] = useState(false);
   
     useEffect(() => {
       const savedMode = localStorage.getItem("darkMode");
@@ -10,25 +11,48 @@ const Acessibilidade = () => {
         setIsDarkMode(true);
       }
     }, []);
-  
+    useEffect(() =>{
+      const savedMode = localStorage.getItem("bigText");
+      if(savedMode == "enabled"){
+        setisBig(true);
+      }
+    }, []);
     useEffect(() => {
       localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
     }, [isDarkMode]);
+    useEffect(() =>{
+      localStorage.setItem("bigText", isBig ? "enabled" : "disabled");
+    }, [isBig]);
     const toggleDarkMode = () => {
       setIsDarkMode((prevMode) => !prevMode);
       window.location.reload();
     };
+    const toggleBigText = () => {
+      setisBig((prevMode) => !prevMode);
+      window.location.reload();
+    }
+   
+
     return (
       <div className={isDarkMode ? "dark-mode" : "body-claro"}>
-        <div>
+        <div className={isBig ? "bigText" : "smallText"}> 
           <header>
             <h1>{isDarkMode ? "Modo Escuro" : "Modo Claro"}</h1>
             <button onClick={toggleDarkMode}>
               Alternar para {isDarkMode ? "Modo Claro" : "Modo Escuro"}
             </button>
+            <h1>Aumentar Texto</h1>
+           <button onClick={toggleBigText}>
+              {isBig ? "Diminuir tamanho texto" : "Aumentar tamanho texto"}
+            </button>
+            Leitura bionica
           </header>
           <main>
-            <p>Este é um exemplo de alternância entre modos.</p>
+          <div>
+              <p>
+                  Acessibilidade
+              </p>
+            </div>
           </main>
         </div>
       </div>

5fc6602a6406ec87ae9c802a67c7c9502c6e8eda
Ana Beatriz <anabeatriz.sureke@gmail.com>
2024-12-02 10:59:04 -0300

    estilização pagina inicial

--- src/pages/Home/home_page.js
+++ src/pages/Home/home_page.js
@@ -4,20 +4,36 @@ import { Title } from "./styles";
 import React, { useState, useEffect, Component } from "react";
 import imagem from "../../assets/lore.png"
 import "./home.css"
+import { textVide } from 'text-vide';
 
 function Home() {
     const [isDarkMode, setIsDarkMode] = useState(false);
+    const[isBig, setisBig] = useState(false);
+    const[bionicReadingActivated, setReading] = useState(false);
     useEffect(() => {
       const savedMode = localStorage.getItem("darkMode");
       if (savedMode === "enabled") {
         setIsDarkMode(true);
       }
     }, []);
-    
+    useEffect(() =>{
+        const savedMode = localStorage.getItem("bigText");
+        if(savedMode == "enabled"){
+          setisBig(true);
+        }
+    }, []);
+      useEffect(() =>{
+        const savedMode = localStorage.getItem("bionicReading");
+        if(savedMode == "enabled"){
+          setReading(true);
+        }
+    },[]);
+  
     return (
-        <div class="container" className={isDarkMode ? 'body-dark-mode' : "body"}>
-            <Title> Bem vindo à página HOME</Title>
-
+        <div class="container" className={isDarkMode ? 'dark-mode' : "body-claro"}>
+            <div className={isBig ? "bigText" : "smallText"}> 
+            <Title> Boas-Vindas a Mythos!</Title>
+            <h4 dangerouslySetInnerHTML={{ __html: bionicReadingActivated ? textVide("Aqui estão algumas das coisas que você pode acessar aqui!") : "Aqui estão algumas das coisas que você pode acessar aqui!"}}></h4>
             <div class="row">
                 <div class="col-md-2">
                     <img src={imagem} width="200" height="200"/>
@@ -66,7 +82,7 @@ function Home() {
                 </div>
                 <br/>
             </div>
-
+            </div>
         </div>
 
 

4227b04e09e094877bc51133c70de456f4187ba5
Ana Beatriz <anabeatriz.sureke@gmail.com>
2024-12-10 08:24:06 -0300

    estilização página de pesquisa

--- src/pages/Pesquisa/pesquisa_page.js
+++ src/pages/Pesquisa/pesquisa_page.js
@@ -1,14 +1,37 @@
 import contos from '../../assets/bd/contos.json'
 import herois from '../../assets/bd/herois.json'
 
-import { useState } from 'react'
+import React, { useState, useEffect} from "react";
 import "./pesquisa.css"
 
 function Pesquisa() {
-
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const[isBig, setisBig] = useState(false);
+    const[bionicReadingActivated, setReading] = useState(false);
+    useEffect(() => {
+      const savedMode = localStorage.getItem("darkMode");
+      if (savedMode === "enabled") {
+        setIsDarkMode(true);
+      }
+    }, []);
+    useEffect(() =>{
+        const savedMode = localStorage.getItem("bigText");
+        if(savedMode == "enabled"){
+          setisBig(true);
+        }
+    }, []);
+      useEffect(() =>{
+        const savedMode = localStorage.getItem("bionicReading");
+        if(savedMode == "enabled"){
+          setReading(true);
+        }
+    },[]);
     const [query, setQuery] = useState("");
     return (
+      <div class="container" className={isDarkMode ? 'dark-mode' : "body-claro"}>
+            <div className={isBig ? "bigText" : "smallText"}> 
         <div className="container">
+          
             <form>
                 <input type="text" placeholder="Search..." className="search" onChange={e => setQuery(e.target.value)} />
             </form>
@@ -23,6 +46,7 @@ function Pesquisa() {
                     ))}
                 </ul>
             </div>
+            </div></div>
         </div>
     )
 }
